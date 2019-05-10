16dfa75e3670db05217cb65f24f07b1bb2daabb2
Arthur Sun <arthursun2005@gmail.com>
2019-05-11 09:49:47 +1000

    Initial Commit

---
+++ Sim3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sim3D
+//
+//  Created by Arthur Sun on 5/11/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
